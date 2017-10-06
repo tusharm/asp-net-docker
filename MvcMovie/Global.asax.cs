58fe45f8d900f1256d54a55896b6a54ffd0a70c2
Tushar Madhukar <tusharm@campaignmonitor.com>
2017-10-06 22:34:36 +1100

    Added Dockerfile + db connection string for an SQL server

--- MvcMovie/Global.asax.cs
+++ MvcMovie/Global.asax.cs
@@ -18,4 +18,4 @@ namespace MvcMovie
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
-}
+}   
---
+++ MvcMovie/docker/enable-iis-management.ps1
@@ -0,0 +1,11 @@
+﻿# create admin user for remote connection
+net user iisadmin "!!Sadmin*" /add
+net localgroup "Administrators" "iisadmin" /add
+
+# install web management features
+Import-Module servermanager
+Add-WindowsFeature web-mgmt-service
+
+# enable remote management
+Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WebManagement\Server -Name EnableRemoteManagement -Value 1
+Start-Service wmsvc
