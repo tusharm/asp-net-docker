c0ac30da23470a0a79a6ee6be3ff61fbb580884c
Tushar Madhukar <tusharm@campaignmonitor.com>
2017-10-09 17:23:02 +1100

    Switched to using bind mounted volumes for project build assets.

---
+++ MvcMovie/docker/web/configure-website.ps1
@@ -0,0 +1,14 @@
+﻿
+param()
+
+Write-Verbose "Creating custom website..."
+
+# create a new drive
+subst G: C:\website
+
+# make the drive mapping permanent
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices' -Name 'G:' -Value '\??\C:\website' -Type String
+
+# add a new website
+Remove-Website -Name 'Default Web Site'
+New-Website -Name 'Website' -PhysicalPath 'G:' -Port 80 -Force
